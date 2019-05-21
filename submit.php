f7e1c6294ecc17237e324f0ce8ce68ad85987d0f
Ifidon <e_fidon@yahoo.com>
2019-05-21 15:22:15 +0100

    may21

--- submit.php
+++ submit.php
@@ -1,8 +1,17 @@
 <?php
   require 'update.php';
 
-  $stmt = $pdo->prepare("UPDATE sub_scr SET grp_scr = :scr");
+  $qry = $pdo->prepare("SELECT grp_id FROM question_grp where grp_category = :cat");
+  $qry->execute(array(
+    ':cat' => $cat
+  ));
+  $gid = $qry->fetch(PDO::FETCH_ASSOC);
+
+
+  $stmt = $pdo->prepare("UPDATE sub_scr SET grp_scr = :scr where user_id = :uid AND grp_id = :gid");
   $stmt->execute(array(
-    'scr' => htmlentities($scr)
+    ':scr' => htmlentities($scr),
+    ':uid' => $id,
+    ':gid' => htmlentities($gid['grp_id'])
   ));
 ?>
